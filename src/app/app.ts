6dc909a47668d382c19efdafb2422b3895111d15
JoshuaIliffe <joshuailiffe8@gmail.com>
2025-12-03 09:46:05 +0100

    Añadido componente bienvenida

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Bienvenida } from './bienvenida/bienvenida';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Bienvenida],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
